1842c254e3a982373d6c21cb6750cdf0749f478d
Slyther Savior <shrijanpoudel9@gmail.com>
2024-05-28 00:14:00 +0545

    debug some minor code issues and call it a day

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
-const { token } = require('./config.json');
-
-require('dotenv').config(); // just calling the environmental variables from the .env file as well !
+require('dotenv').config(); // Load environmental variables from the .env file as well
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const token = process.env.DISCORD_TOKEN;
 
 // When the client is ready, run this code (only once).
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    
+    // Set the bot status to Watching with happiness
+    client.user.setPresence({ 
+        activities: [{
+            name: 'with happiness',
+            type: ActivityType.Watching,
+            url: 'https://github.com/SlytherSavior/Test-Bot'
+        }],
+        status: 'dnd'
+    });
 });
-
-//set the bot status to Waching with happiness ! 
-client.user.setPresence({ 
-  activities: [{
-    name: 'with happiness',
-    type: ActivityType.Watching,
-    url: 'https://github.com/SlytherSavior/Test-Bot'
-  }],
-  status: 'dnd'
-})
-
 
 // Log in to Discord with your client's token
 client.login(token);
-
