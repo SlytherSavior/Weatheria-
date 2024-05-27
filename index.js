d54e0b4fb6fa05b91753496c0b6e71ffbc54b830
Slyther Savior <shrijanpoudel9@gmail.com>
2024-05-28 00:08:30 +0545

    update bot status functions , include ActivitiyType from discordjs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
+
 require('dotenv').config(); // just calling the environmental variables from the .env file as well !
 
 // Create a new client instance
@@ -10,6 +11,18 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+//set the bot status to Waching with happiness ! 
+client.user.setPresence({ 
+  activities: [{
+    name: 'with happiness',
+    type: ActivityType.Watching,
+    url: 'https://github.com/SlytherSavior/Test-Bot'
+  }],
+  status: 'dnd'
+})
+
+
 // Log in to Discord with your client's token
 client.login(token);
 
