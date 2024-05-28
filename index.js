a58454bf01e9bb4aa525ee8f29e34906bee9c3b2
Slyther Savior <shrijanpoudel9@gmail.com>
2024-05-29 01:32:53 +0545

    try to work along with the weather api and also send message

--- index.js
+++ index.js
@@ -2,11 +2,49 @@
 const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 require('dotenv').config(); // Load environmental variables from the .env file as well
 const https = require("https");
-const weatherApi = process.env.WEATHER_API_KEY; 
 
 // // Create a new client instance
 // const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // const token = process.env.DISCORD_TOKEN;
+const weatherApi = process.env.WEATHER_API_KEY; 
+
+//fucntion for getting weather data
+function setUrl(cityName) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${weatherApi}`;
+    return url;
+}
+
+
+function getWeatherData(cityname, callback) { 
+    const url = setUrl(cityname) ;
+
+    https.get(url, (resp) => { 
+        let data = ''; 
+
+        // A chunk of data has been recieved 
+        resp.on('data', (chunk) => { 
+            data += chunk; 
+        }); 
+
+
+        //The whole response has been recieved 
+        resp.on('end', () => { 
+            if(resp.statusCode === 200 ) { 
+                const weatherData = JSON.parse(data); 
+                const temp = weatherData.main.temp; 
+                const description = weatherData.weaather.description; 
+                callback(null, weatherData) ; 
+            }else { 
+                callback(`Error ${resp.statusCode} - ${resp.statusMessage}`);
+            }
+        }).on('error', (err) => { 
+            callback(err);
+        })
+
+
+    })
+
+}
 
 // // When the client is ready, run this code (only once).
 // client.once(Events.ClientReady, readyClient => {
@@ -22,25 +60,3 @@ const weatherApi = process.env.WEATHER_API_KEY;
 //         status: 'dnd'
 //     });
 // });
-
-
-
-
-// // Log in to Discord with your client's token
-// client.login(token);
-
-
-//here we will try out our current codes . ! 
-// adding api from openweather , for getting weather  ! 
-
-console.log(weatherApi); 
-
-
-
-
-//fucntion for getting weather data
-function setUrl(cityName) {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${weatherApi}`;
-    return url;
-}
-
