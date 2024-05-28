b08f6b6b709491ab74e42dbff9ef2ff183551c13
Slyther Savior <shrijanpoudel9@gmail.com>
2024-05-29 03:03:32 +0545

    final commit of the day, goal tomorrow..dayafter : fix the bug and get the app up and running, to add other extra commands and work on other functiosn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, Events } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Events, ActivityType } = require('discord.js');
 require('dotenv').config(); // Load environmental variables from the .env file as well
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -26,14 +26,27 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+
+
+    //set the user activity 
+
+    client.user.setPresence({
+        activities: [{
+            name: 'with happiness',
+            type: ActivityType.Competing, 
+            url: 'https://github.com/SlytherSavior/Test-Bot'
+        }],
+    })
 });
+
+
 
 // Handle command interactions
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
     console.log(interaction);
 
-    const command = interaction.client.commands.get(interaction.commandName);
+    	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found.`);
