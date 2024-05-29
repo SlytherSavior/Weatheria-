f51405a8fad867ebcc1b9dcbcf630ba16f30325a
Slyther Savior <shrijanpoudel9@gmail.com>
2024-05-29 18:26:00 +0545

    feat: debuged the error, now the commands were not registered previously , now they are , so let's add more functionality tomorrow

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const { Client, Collection, GatewayIntentBits, Events, ActivityType } = require(
 require('dotenv').config(); // Load environmental variables from the .env file as well
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Commands collection
-client.commands = new Collection();
+client.commands = new Collection(); 
 
-// Path to commands directory
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath);
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-// Loading commands
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-   // Set a new item in the Collection with the key as the command name and the value as the exported module
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
-    }
+	}
+}
 
 
 client.once('ready', () => {
