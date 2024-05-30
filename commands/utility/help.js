fb4e0c2c316d2f8db67218f91e8eba74e925136f
Slyther Savior <shrijanpoudel9@gmail.com>
2024-05-30 21:51:19 +0545

    added help command

---
+++ commands/utility/help.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Displays information about available commands.'),
+    async execute(interaction) {
+        const commands = interaction.client.commands;
+
+        // Format the list of commands
+        const commandList = commands.map(command => `/${command.data.name}: ${command.data.description}`).join('\n');
+
+        // Send the list of commands as a reply
+        interaction.reply(`**Available Commands:**\n${commandList}`);
+    },
+};
